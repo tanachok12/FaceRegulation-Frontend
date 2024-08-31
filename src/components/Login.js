96e1e07ba47b87cf9c6aa5864a70b393185a47b9
Tanachoknaja <tanachok_d@elearning.cmu.ac.th>
2024-08-31 12:29:56 +0700

    Test HTTPS โดยไม่ตรวจสอบ

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Login.css'; // Import the CSS file
+const https = require('https');
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-
+  const agent = new https.Agent({  
+    rejectUnauthorized: false
+  });
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
   
